cc884ec9ec4b009b7a4955569a2a2d918c1472d7
Rithik Sai <68235912+rithiksai2002@users.noreply.github.com>
2025-03-10 17:16:17 +0530

    added new logics and features to the web page

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -8,13 +8,13 @@ export default function TextForm(props) {
     setText(changedText);
   };
 
+  // This is used to listen i.e to notice that if any changes are done to the text area 
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
   };
 
-  const handleLowerCase =()=>
-  {
+  const handleLowerCase = () => {
     console.log("clicked on the Lower case button")
     setText(prevText => prevText.toLowerCase())
   }
@@ -23,23 +23,23 @@ export default function TextForm(props) {
   //  Wrong Way to change the state
   //  text =  "Demo text"
   return (
-    <div>
-      <h1>{props.heading} </h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          onChange={handleOnChange}
-          value={text}
-        ></textarea>
-      </div>
-      <button className="btn btn-primary mx -3" onClick={handleText}>
-        Convert to Upper Case
-      </button>
-      <button className = "btn btn-primary mx-3" onClick = {handleLowerCase}>
-        Convert to Lower Case
-      </button>
+    <>
+    <div className = "container">
+          <h1>{props.heading} </h1>
+          <div className="mb-3">
+               <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" onChange={handleOnChange} value={text}  ></textarea>
+          </div>
+          <button className="btn btn-primary mx -3" onClick={handleText}>  Convert to Upper Case
+          </button>
+          <button className="btn btn-primary mx-3" onClick={handleLowerCase}>  Convert to Lower Case
+          </button>
     </div>
+    <div className="container my-3">
+         <h3>Your Text Summary</h3>
+         <p>{(text.split(" ")).length -1} words, {text.length} characters</p>
+         <p>{0.008 * ((text.split(" ")).length -1)} minutes to read</p>
+      
+    </div>
+    </>
   );
 }
